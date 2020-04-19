e2a24b2a0d0cbdcaecf2b37ef35118e1b32230cb
Adrian Etenyi <etadriano2@gmail.com>
2020-04-19 11:15:55 +0300

    add function for creating a new user

--- user.py
+++ user.py
@@ -8,3 +8,10 @@ class User:
         save_user method saves users
         '''
         User.user_list.append(self)
+    def __init__(self,first_name,second_name,password):
+
+       #create new user instance
+
+        self.first_name = first_name
+        self.second_name = second_name
+        self.password = password
