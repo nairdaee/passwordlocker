dd2133862d79e1ea2600fb74754e709273eef9fc
Adrian Etenyi <etadriano2@gmail.com>
2020-04-19 13:11:07 +0300

    add run file for executing app

---
+++ run.py
@@ -0,0 +1,26 @@
+from user import User
+
+def create_user(f_name,s_name,password):
+    '''
+    function to create a new user
+    '''
+    new_user = User(f_name,s_name,password)
+    return new_user
+def save_users(user):
+    '''
+    function to save a user
+    '''
+    user.save_user()
+def display_users():
+    '''
+    function that dispalys all signed up users
+    '''
+    return User.display_users()
+def intro():
+    print("Hey there! Welcome to Password Locker")
+    print('\n')
+    print("Please sign up for an accout to enjoy services")
+
+if __name__ == '__main__':
+
+    intro()
