0ee082224ec05c10cba065dc81ab782ba6638681
Adrian Etenyi <etadriano2@gmail.com>
2020-04-19 13:27:28 +0300

    add function to create a new credential

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
 from user import User
+import string
+import random
+import getpass
+import pyperclip
+from credentials import Credential
 
+def create_credential(account_name,passkey):
+    '''
+    function to create a new credential
+    '''
+    new_credential = Credential(account_name,passkey)
+    return new_credential
+    
 def create_user(f_name,s_name,password):
     '''
     function to create a new user
