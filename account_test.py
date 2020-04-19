c687185d551912102363736ba49970c8db2f40e8
Adrian Etenyi <etadriano2@gmail.com>
2020-04-19 11:39:45 +0300

    cache files

--- account_test.py
+++ account_test.py
@@ -20,6 +20,26 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.first_name,"Adrian")
         self.assertEqual(self.new_user.second_name,"Etenyi")
         self.assertEqual(self.new_user.password,"Mutemuas2001")
+    def test_save_user(self):
+        '''
+        test_save_user tests if a new user created is saved
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),3)
 
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user tests if many users can be saved
+        '''
+        self.new_user.save_user()
+        test_user = User("test","user","muk")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+
+    def test_display_users(self):
+        '''
+        method that displays all thes signed up users
+        '''
+        self.assertEqual(User.display_users(),User.user_list)
 if __name__ == '__main__':
     unittest.main()
